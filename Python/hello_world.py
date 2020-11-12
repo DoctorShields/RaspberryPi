c5f0a62e2c849e970f18a464db8290c8d7e74efb
Dr. Shields <shieldm1@charlottesvilleschools.org>
2020-11-12 18:04:05 +0000

    just messing around

--- Python/hello_world.py
+++ Python/hello_world.py
@@ -1,1 +1,13 @@
-print("hello world!")
+import math as i
+j = i.sin
+print(j(i.pi/4))
+blah = 9
+state = False
+blah = 1 if state else 3
+print(blah)
+'''j("hello world!")
+for i in range(100):
+  if i<40 or i>50:
+    j(i)
+
+'''
