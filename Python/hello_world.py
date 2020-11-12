d79cb1f077c073c43525c2d94bf5eda05023d1ff
Dr. Shields <shieldm1@charlottesvilleschools.org>
2020-11-12 18:39:12 +0000

    abby updated

--- Python/hello_world.py
+++ Python/hello_world.py
@@ -1,13 +1,5 @@
-import math as i
-j = i.sin
-print(j(i.pi/4))
-blah = 9
-state = False
-blah = 1 if state else 3
-print(blah)
-'''j("hello world!")
-for i in range(100):
-  if i<40 or i>50:
-    j(i)
+# my hello world assignment
 
-'''
+for i in range(10):
+  print("Hello world!")
+  print("hi!")
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RaspberryPi
+
+## Python Program 01 – Calculator
+
+What I did
+
+## Python Program 02 - Quadratic solver
+
+What I did
