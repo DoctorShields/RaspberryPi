0083effc3703753ac757c0782f3e5ccbe7bba03e
bwebste20 <bwebste20@charlottesvilleschools.org>
2021-06-09 15:24:47 +0100

    moving eye

--- Python/eve/lcd_test.py
+++ Python/eve/lcd_test.py
@@ -1,6 +1,7 @@
 import board
 import busio
 import time
+from math import floor
 
 from adafruit_is31fl3731.matrix import Matrix as Display
 
@@ -8,29 +9,18 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 display = Display(i2c)
 
+rows = 9
+cols = 16
+drop = 40
+
 def displayPixels(xr):
-#	print(xr)
-	for x in range(0,16):
-		for y in range(0,9):
-			if x == xr:
-				val = 100
-			else:
-				val = 0
-			val = 255-abs(x-xr)*32
+	for x in range(0,cols):
+		for y in range(0,rows):
+			val = 255-abs(x-xr)*drop-abs(floor(rows/2)-y)*drop
 			if val < 0:
 				val = 0
 			display.pixel(x,y,val)
 #			print(val)
-'''
-			if abs(x-xr) == 1:
-				display.pixel(x,y,100)
-			elif abs(x-xr) == 2:
-				display.pixel(x,y,10)
-			elif x == xr:
-				display.pixel(x,y,255)
-			else:
-				display.pixel(x,y,0)
-'''
 row = 1
 dr = 1
 
